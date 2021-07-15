ecce6d95c24a9ece4cf9acc7ceb9f26532fb14dc
DKNNYEL <83793990+DKNNYEL@users.noreply.github.com>
2021-07-15 10:47:08 -0500

    Add files via upload
    un breve tutorial del grafico espiral

---
+++ spiregraphs.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 
+# ## Grafico Spiral
+
+# In[1]:
+
+
+from turtle import *
+
+
+# In[2]:
+
+
+speed(0)
+bgcolor("black")
+pensize(2)
+
+for i in range(6):
+    for colours in ["red", "magenta","blue", "cyan","yellow", "green", "white"]:
+        color(colours)
+        circle(100)
+        left(10)
+        
+mainloop()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
